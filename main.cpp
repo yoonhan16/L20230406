3a7c74c409a0ea34f42040271ee4e428cb16e4c4
YoonHan KIM <reprica123@gmail.com>
2023-04-06 11:30:36 +0900

    플레이어 생성

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "Player.h"                             // 만든 클래스를 넣을 때
+
 
 using namespace std;
 
 int main()   // entry point 이거 없으면 뭘 해도 안됨
 {
+	int* P = new int();
 
+	FPlayer* Player = new FPlayer();             // 몬스터도 똑같이 만들면 됨
+
+
+
+	delete Player;                               // 플레이어가 죽은 경우
 
 	return 0;
 }
